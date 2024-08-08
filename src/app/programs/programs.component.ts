fc3e5442dcf2550519ddf70f7f3e4b6eab1dfdd4
olajide <olajidecrown@gmail.com>
2024-08-08 04:03:23 +0100

    added slder

--- src/app/programs/programs.component.ts
+++ src/app/programs/programs.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
+import { Options } from '@angular-slider/ngx-slider';
+
 @Component({
   selector: 'app-programs',
   standalone: true,
-  imports: [NgbAccordionModule, RouterModule],
+  imports: [NgbAccordionModule, RouterModule, NgxSliderModule],
   templateUrl: './programs.component.html',
   styleUrl: './programs.component.scss'
 })
 export class ProgramsComponent {
+  minValue: number = 5000;
+  maxValue: number = 58000;
+  options: Options = {
+    floor: 0,
+    ceil: 70000
+  };
   constructor(
     private router: Router
   ) { }
